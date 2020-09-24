127a51315e3480b22d0f5e72f6f43a19733d5996
philibert-tech <philibert.lisette@hangarww.com>
2020-09-24 16:35:57 +0400

    Created Product filter

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -80,7 +80,7 @@ namespace MyShop.WebUI.Controllers
             else
             {
                 ProductManagerViewModel viewModel = new ProductManagerViewModel();
-                viewModel.Product = new Product();
+                viewModel.Product = product;
                 viewModel.ProductCategories = productCategories.Collection();
                 return View(viewModel);
             }
