5c6cb8ef3d6af44795d34e2ba3e1ae6e7f746a4e
philibert-tech <philibert.lisette@hangarww.com>
2020-09-28 14:48:09 +0400

    Order Manager added

--- MyShop/MyShop.Services/OrderService.cs
+++ MyShop/MyShop.Services/OrderService.cs
@@ -35,5 +35,25 @@ namespace MyShop.Services
             orderContext.Insert(baseOrder);
             orderContext.Commit();
         }
+
+        public List<Order> GetOrderList()
+        {
+            return orderContext.Collection().ToList();
+        }
+
+
+
+        public Order GetOrder(string id)
+        {
+            return orderContext.Find(id);
+        }
+
+
+        public void UpdateOrder(Order updateOrder)
+        {
+            orderContext.Update(updateOrder);
+            orderContext.Commit();
+        }
+
     }
 }
---
+++ MyShop/MyShop.WebUI/Controllers/OrderManagerController.cs
@@ -0,0 +1,58 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class OrderManagerController : Controller
+    {
+        IOrderService orderService;
+
+        public OrderManagerController(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+        // GET: OrderManager
+        public ActionResult Index()
+        {
+            List<Order> orders = orderService.GetOrderList();
+            return View(orders);
+        }
+
+        public ActionResult GetOrder(string Id)
+        {
+            Order order = orderService.GetOrder(Id);
+            return View(order);
+        }
+
+        public ActionResult UpdateOrder(string Id)
+        {
+            ViewBag.StatusList = new List<string>() {
+            "Order Created",
+            "Payment Processed",
+            "Order Shipped",
+            "Order Complete"
+            };
+            Order order = orderService.GetOrder(Id);
+            return View(order);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateOrder(Order UpdateOrder, string Id)
+        {
+            Order order = orderService.GetOrder(Id);
+
+            order.OrderStatus = UpdateOrder.OrderStatus;
+            orderService.UpdateOrder(order);
+
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
